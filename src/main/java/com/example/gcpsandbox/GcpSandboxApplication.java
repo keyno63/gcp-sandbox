3234a08a7cb3f4cdea6c30a63994b86e72e85845
Keyno <kofujiw@gmail.com>
2022-04-02 10:10:09 +0900

    Initial commit

---
+++ src/main/java/com/example/gcpsandbox/GcpSandboxApplication.java
@@ -0,0 +1,14 @@
+
+package com.example.gcpsandbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GcpSandboxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GcpSandboxApplication.class, args);
+    }
+
+}
